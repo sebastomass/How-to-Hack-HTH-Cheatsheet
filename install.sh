70643010cbf0c8265666bf4c72316c59f6c6ca5b
sebastomass <sebastomassantacruz@gmail.com>
2024-04-20 15:48:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-# Descargar el script principal desde GitHub
-# wget https://raw.githubusercontent.com/sebastomass/How-to-Hack-HTH-Cheatsheet/main/hth.sh
-
-# Dar permisos de ejecución al script descargado
-chmod +x hth.sh
-
-# Mover el script a un directorio donde esté en el PATH del usuario
-mv hth.sh /usr/local/bin/
-
-# Agregar el alias al archivo de perfil de shell
-echo "alias hth=\"hth.sh\"" >> ~/.bashrc
-
-# Recargar el archivo de perfil de shell para que los cambios surtan efecto
-source ~/.bashrc
-
+wget https://raw.githubusercontent.com/sebastomass/How-to-Hack-HTH-Cheatsheet/main/hth
+chmod +x hth
+mv hth /usr/bin/
 echo "HTH ha sido instalado correctamente."
